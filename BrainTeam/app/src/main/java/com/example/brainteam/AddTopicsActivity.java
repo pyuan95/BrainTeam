2cc9ffb398eab8f09c1cd0a56aa9691eda29838d
IHSSAT <patrick.yuan@icsd.k12.ny.us>
2019-03-03 21:03:03 -0500

    add delete buttons

--- BrainTeam/app/src/main/java/com/example/brainteam/AddTopicsActivity.java
+++ BrainTeam/app/src/main/java/com/example/brainteam/AddTopicsActivity.java
@@ -4,17 +4,33 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class AddTopicsActivity extends AppCompatActivity {
 
+
+    private static List<String> items = new ArrayList<String>();
+    ListView myLV;
+    ArrayAdapter myAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_topics);
+        String[] from = {"item", "deleteButton"};
+        // int[] to = {R.id.listview_image, R.id.listview_item_title};
+        myAdapter = new ArrayAdapter(this, R.layout.topic_list, R.id.topicName, items);
+        myLV = (ListView) findViewById(R.id.topicList);
+        myLV.setAdapter(myAdapter);
+
     }
 
     public void onAddTopicToList(View view)
@@ -22,13 +38,21 @@ public class AddTopicsActivity extends AppCompatActivity {
         EditText addTopicToList = (EditText) findViewById(R.id.addTopicToList);
         String topic = addTopicToList.getText().toString();
         addTopicToList.setText("");
-        LinearLayout topicList = (LinearLayout) findViewById(R.id.topicList);
-        TextView newTopic = new TextView(AddTopicsActivity.this);
-        newTopic.setText(topic);
-        newTopic.setTextColor(Color.BLACK);
-        newTopic.setTextSize(18);
-        newTopic.setPadding(0,16, 0 ,16);
-        topicList.addView(newTopic);
+        items.add(topic);
+        myAdapter.notifyDataSetChanged();
+
+
+//        TextView newTopic = new TextView(AddTopicsActivity.this);
+//        newTopic.setText(topic);
+//        newTopic.setTextColor(Color.BLACK);
+//        newTopic.setTextSize(18);
+//        newTopic.setPadding(0,16, 0 ,16);
+//        topicList.addView(newTopic);
+    }
+
+    public void onDeleteTopic(View view)
+    {
+
     }
 
 
@@ -37,7 +61,7 @@ public class AddTopicsActivity extends AppCompatActivity {
         new Thread()
         {
             DatabaseManager db = new DatabaseManager(AddTopicsActivity.this, true);
-            LinearLayout topicList = (LinearLayout) findViewById(R.id.topicList);
+            ListView topicList = (ListView) findViewById(R.id.topicList);
             public void run()
             {
                 int i = 0;
@@ -59,8 +83,6 @@ public class AddTopicsActivity extends AppCompatActivity {
                     }
                 }
             }
-
-
         };
     }
 }
